9e32967c0fcc87dd7e61d87e3aca4772c842fdfb
Alexander Harris <aph1102@jagmail.southalabama.edu>
2022-10-12 15:29:05 -0500

    sync

--- components/ItemDetails.tsx
+++ components/ItemDetails.tsx
@@ -1,21 +1,48 @@
 import axios from "axios";
+import { useRouter } from "next/router";
 import React from "react";
 import { Client, Environment } from "square";
 
-const ItemDetails = ({ itemId, data }: any) => {
+const ItemDetails = ({ itemId }) => {
   // console.log("here it is", data.data);
-  const itemData = data.data.itemData;
-  console.log("test", itemData);
+  // const itemData = data.data.itemData;
+  // console.log("test", itemData);
+  const router = useRouter();
+  console.log("fuck", router.query.itemId);
+  console.log("fuck this");
 
   return (
     <>
-      <div>ItemDetails is {itemId}</div>
-      <h1> this is a page for {itemData.name}</h1>
-      <h1>
-        this is the desc of {itemData.name}: {itemData.description}
-      </h1>
+      <div>ItemDetails is {router.query.itemId}</div>
     </>
   );
+};
+
+export const getServerSideProps = async (ctx) => {
+  const itemId = ctx.query.itemId;
+  console.log("bro");
+  console.log(itemId);
+  const config = {
+    accessToken: process.env.SQUARE_ACCESS_TOKEN,
+    environment: Environment.Sandbox,
+  };
+
+  const { catalogApi } = new Client(config);
+
+  try {
+    const response = await catalogApi.retrieveCatalogObject(itemId, true);
+    const data = JSON.parse(JSON.stringify(response.result));
+    const relData = JSON.parse(JSON.stringify(response.result.relatedObjects));
+    console.log("in itemDetail", response.result);
+    return {
+      props: {
+        data: data,
+        relData: relData,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default ItemDetails;
--- pages/menu/[ProductId].tsx
+++ pages/menu/[ProductId].tsx
@@ -21,28 +21,28 @@ export default function ProductId() {
   );
 }
 
-export const getServerSideProps = async (context: any) => {
-  const { itemId } = context.query;
-  console.log(context.query);
-  const config = {
-    accessToken: process.env.SQUARE_ACCESS_TOKEN,
-    environment: Environment.Sandbox,
-  };
+// export const getServerSideProps = async (context: any) => {
+//   const { itemId } = context.query;
+//   console.log(context.query);
+//   const config = {
+//     accessToken: process.env.SQUARE_ACCESS_TOKEN,
+//     environment: Environment.Sandbox,
+//   };
 
-  const { catalogApi } = new Client(config);
+//   const { catalogApi } = new Client(config);
 
-  try {
-    const response = await catalogApi.retrieveCatalogObject(itemId, true);
-    const data = JSON.parse(JSON.stringify(response.result.object));
-    const relData = JSON.parse(JSON.stringify(response.result.relatedObjects));
-    console.log("in PID", relData);
-    return {
-      props: {
-        data: data,
-        relData: relData,
-      },
-    };
-  } catch (error) {
-    console.log(error);
-  }
-};
+//   try {
+//     const response = await catalogApi.retrieveCatalogObject(itemId, true);
+//     const data = JSON.parse(JSON.stringify(response.result.object));
+//     const relData = JSON.parse(JSON.stringify(response.result.relatedObjects));
+//     console.log("in PID", relData);
+//     return {
+//       props: {
+//         data: data,
+//         relData: relData,
+//       },
+//     };
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
